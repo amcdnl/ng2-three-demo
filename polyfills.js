a06cedb99a0ba3193fd9019d711c5812ad1d7180
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-16 09:21:42 -0400

    Updates

--- polyfills.js
+++ polyfills.js
@@ -86,7 +86,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "4b8626aed95378e1e8f7"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "8100736f8f868974834d"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
